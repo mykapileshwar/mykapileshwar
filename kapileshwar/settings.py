625fa069d88b47b65c0691357de109e90e9c75f3
onkargh <43819311+Onkar-Gh@users.noreply.github.com>
2022-01-27 19:01:55 +0530

    Change feedback email

--- kapileshwar/settings.py
+++ kapileshwar/settings.py
@@ -141,11 +141,11 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
-EMAIL_HOST_USER = "testingapp1950@gmail.com"
+EMAIL_HOST_USER = "kapileshwar.info@gmail.com"
 EMAIL_HOST_PASSWORD = os.environ['EMAIL_HOST_PASSWORD']
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
-DEFAULT_FROM_EMAIL = "testingapp1950@gmail.com"
+DEFAULT_FROM_EMAIL = "kapileshwar.info@gmail.com"
 
 CSRF_COOKIE_SECURE = True
 SESSION_COOKIE_SECURE = True
